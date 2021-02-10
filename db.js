783a291ba97749c452e028b57aae56c35a8db7cf
debesteste <gersonlynch@gmail.com>
2021-02-10 19:37:25 +0100

    Changed database uri from localhost to aws(heroku)

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const databaseURL = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres';
+const databaseURL = 'postgres://yffprazuilurai:8668db1837c7e4dfddb743fcc35f171c721e12ef75016402739e9f20daf090c1@ec2-35-174-118-71.compute-1.amazonaws.com:5432/da03jpkkhgrnk4' || 'postgres://postgres:secret@localhost:5432/postgres';
 const db = new Sequelize(databaseURL);
 
 module.exports = db;
